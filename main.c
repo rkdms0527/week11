7fdac7bcbfff620a35319e3b3011368aa36cf295
rkdms0527 <71059810+rkdms0527@users.noreply.github.com>
2020-11-12 15:45:39 +0900

    3

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void)
-	{
-		int i = 10;
-		int *ptr = &i;
-		printf("%i = %d, pi : %p\n", i, ptr);
-		(*ptr)++;
-		printf("%i = %d, pi : %p\n", i, ptr);
-		*ptr++;
-		printf("%i = %d, pi : %p\n", i, ptr);
+{
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+	**q = 300;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+	
 	return 0;
 }
